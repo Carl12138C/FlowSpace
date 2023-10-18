6e6f291896de2c279b70f2d880cbb2fd58f0c4c4
Carl <shiyangcai12138@gmail.com>
2023-10-18 15:40:54 -0400

    added Tile and components for testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-import Tile from './components/Tile'
+import Tile from './components/Tile.jsx'
 
 function App() {
 
   return (
     <>
       <div>Hello Mom!</div>
+      <Tile></Tile>
     </>
   )
 }
