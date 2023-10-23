208bb1417fef18b01494af2fcdf44d84d3313ad7
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2023-10-23 17:04:30 -0400

    clean up initial code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css'
-import Tile from './components/Tile'
-import ChatLog from './components/chatPage/ChatLog'
 
 function App() {
 
   return (
     <>
-      {/* <Tile name="John" tid="123" size="M"/> */}
-      <ChatLog/>
       <div>Hello Mom!</div>
     </>
   )
