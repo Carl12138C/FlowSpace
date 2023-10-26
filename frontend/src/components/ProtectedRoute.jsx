7b9f72856ee19fed356321745b192bf9daedea23
Carl <shiyangcai12138@gmail.com>
2023-10-26 19:38:14 -0400

    renamed protected route and commented console

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { getUserContext } from "../context/AuthContext";
 export default function ProtectedRoute({children}) {
     const authData = getUserContext();
 
-    console.log(authData);
+    // console.log(authData);
     return(
         <>
             {( authData.userData ) ? children : <Navigate to="/"/>}
