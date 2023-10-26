18e1c7899d2919a5ca070e131dac9f6840c28fcb
Carl <shiyangcai12138@gmail.com>
2023-10-26 19:34:11 -0400

    added route structure and implementd protected route

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createContext, useContext, useState } from "react";
+
+const UserContext = createContext(null);
+
+export function AuthContext({ children }) {
+    const [userData, setUserData] = useState();
+
+    return(
+        <UserContext.Provider value={{userData, setUserData}}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+export function getUserContext() {
+    return useContext(UserContext);
+}
