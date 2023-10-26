ddaf26abde6e04d3c243f73c63078d63fafaf41b
Carl <shiyangcai12138@gmail.com>
2023-10-26 19:39:07 -0400

    renamed protectedroute

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router";
-import ProtectedRoute from "../components/ProtectRoute";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 export default function MainLayout() {
   return (
