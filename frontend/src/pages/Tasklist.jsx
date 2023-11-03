6a2ffe01c926816e09dd6aa1dddb5dd84a804b07
Andre D'sa <andrehdsa@gmail.com>
2023-11-03 19:05:48 -0400

    Adding styles to TaskList and TaskItems

--- frontend/src/pages/Tasklist.jsx
+++ frontend/src/pages/Tasklist.jsx
@@ -1,9 +1,32 @@
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import TaskItem from '../components/TaskItem';
 
-export default function Tasklist() {
-    
-    return(
-        <>
-            <h3>Task list</h3>
-        </>
+const StyledDiv = styled('div')({
+  backgroundColor: 'aliceblue',
+  marginLeft: 20,
+  width: 100,
+  borderRadius: 4,
+});
+
+export default function Tasklist() {    
+    return (
+      <StyledDiv>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={1}>
+            <Grid xs={12}>
+              <Stack spacing={2}>
+                <TaskItem />
+                <TaskItem />
+                <TaskItem />
+                <TaskItem />
+                <TaskItem />
+              </Stack>
+            </Grid>
+          </Grid>
+        </Box>
+      </StyledDiv>
     );
 }
