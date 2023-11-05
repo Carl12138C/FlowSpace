5a5b5d51324985bbe6c723135d1ab0af111be868
Andre D'sa <andrehdsa@gmail.com>
2023-11-05 18:02:47 -0500

    Finished basic task input modal (User Story 6)

--- frontend/src/components/TaskInput.jsx
+++ frontend/src/components/TaskInput.jsx
@@ -1,8 +1,17 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Modal,
+  OutlinedInput,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 
 const style = {
@@ -17,10 +26,27 @@ const style = {
   p: 4,
 };
 
+const names = [
+    "Andre",
+    "Carl",
+    "Kevin"
+];
+
 export default function TaskInput() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
 
   return (
     <div>
@@ -41,12 +67,36 @@ export default function TaskInput() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Create A Task
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Input fields will go here.
-          </Typography>
+          <Stack spacing={2}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Create A Task
+            </Typography>
+            <FormControl fullWidth sx={{ m: 1 }}>
+              <InputLabel id="name-selector-label">Assignees</InputLabel>
+              <Select
+              labelId="name-selector-label"
+                id="name-selector"
+                multiple
+                value={personName}
+                onChange={handleChange}
+                input={<OutlinedInput label="Assignees" />}
+              >
+                {names.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    {name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <TextField fullWidth label="Title" id="new-task-title" />
+            <TextField
+              fullWidth
+              id="new-task-des"
+              label="Description"
+              multiline
+              rows={4}
+            />
+          </Stack>
         </Box>
       </Modal>
     </div>
