bf36dc139da84ae9126852bb42ce2e2dbffe436f
Andre D'sa <andrehdsa@gmail.com>
2023-11-05 20:16:48 -0500

    New styling for the homepage and added new logo (#6)
    * New styling for the homepage and added new logo
    
    * Added package..json and packagelock.json to changed files
    
    ---------
    
    Co-authored-by: Kevin27954 <98406775+Kevin27954@users.noreply.github.com>

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { getUserContext } from "../context/AuthContext";
 import { useNavigate } from "react-router";
+import { Avatar, Box, Card, CardContent, Stack, TextField, Typography } from "@mui/material";
+import Kirby from "../../../image/Desk_Kirby.png"
 
 export default function Home() {
   const UserContext = getUserContext();
@@ -11,10 +13,35 @@ export default function Home() {
   }
   return (
     <>
-      <h1>This is Home</h1>
-      <button onClick={login} style={{height: "20px"}}>
-        Login
-      </button>
+      <Box
+        sx={{
+          width: "100vw",
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, width: 150, height: 150 }} src={Kirby} />
+        <Card>
+          <CardContent>
+            <Typography
+              sx={{ fontSize: 28 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              Welcome to FlowSpace
+            </Typography>
+            <Stack spacing={2}>
+              <TextField label="Username" />
+              <TextField label="Password" />
+              <button onClick={login} style={{ height: "20px" }}>
+                Login
+              </button>
+            </Stack>
+          </CardContent>
+        </Card>
+      </Box>
     </>
   );
 }
