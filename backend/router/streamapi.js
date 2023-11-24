8b70234d7ab2802e752d6fd42b56a082dc203a21
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2023-11-24 14:51:20 -0500

    created login endpoints for application and setup user login information to streamapi on chatpage.jsx

--- backend/router/streamapi.js
+++ backend/router/streamapi.js
@@ -1,10 +1,28 @@
 const express = require("express")
+const StreamChat = require('stream-chat').StreamChat;
 require("dotenv").config()
 
 const streamAPIRouter = express.Router();
+const cilent = StreamChat.getInstance(process.env.streamKey, process.env.streamSecretKey)
 
-streamAPIRouter.use("/user/:id", function incoming(req, res) {
-    res.send("User Id: " + req.params.id);
+streamAPIRouter.get("/user", async function incoming(req, res) {
+    var d = new Date()
+    d.setMonth(d.getMonth() - 1);
+    d.setHours(0, 0, 0, 0);
+
+    var usersList = []
+    const users = await cilent.queryUsers({last_active: {$gt: d} },[{last_active: -1}]);
+    
+    users.users.map((user) => {
+        if(user.name == "liukevin.nyc@gmail.com") {
+            console.log(user);
+        } else {
+            usersList.push(user.name);
+        }
+        // cilent.deleteUsers()
+    })
+
+    res.json(usersList);
 })
 
 module.exports = streamAPIRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const firebaseRouter = require('./router/firebase.js');
-const streamAPIRouter = require('./router/streamapi.js')
+const streamAPIRouter = require('./router/streamapi.js');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+var corsOptions = {
+    origin: "http://localhost:5173"
+}
+
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+
 app.use('/firebase', firebaseRouter);
-app.use('streamapi', streamAPIRouter);
+app.use('/streamapi', streamAPIRouter);
 
 app.listen('3000', function callBack() {
     console.log('Server live on http://localhost:3000');
